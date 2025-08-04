509e93f2da803a01801fd4450538e021420696f9
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-08-03 23:20:06 -0300

    first commit

---
+++ src/main/java/br/com/dev/osorio/full_curse_spring_boot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.dev.osorio.full_curse_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/osorio/full_curse_spring_boot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.osorio.full_curse_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
