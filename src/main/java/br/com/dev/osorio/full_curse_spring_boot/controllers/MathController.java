c453cb6faae459e662ab396d96c64e3de2cf6bd0
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-08-06 04:46:34 -0300

    feat: create mathController with sum endpoint for the learning of @PathVariable

---
+++ src/main/java/br/com/dev/osorio/full_curse_spring_boot/controllers/MathController.java
@@ -0,0 +1,35 @@
+package br.com.dev.osorio.full_curse_spring_boot.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+
+        String number = strNumber.replace(',', '.');
+
+        return Double.parseDouble(number);
+    }
+
+    private Boolean isNumeric(String strNumber) {
+
+        if (strNumber == null || strNumber.isEmpty()) return false;
+
+        String number = strNumber.replace(',', '.');
+        return number.matches("[-+]?\\d*\\.?\\d+");
+    }
+}
