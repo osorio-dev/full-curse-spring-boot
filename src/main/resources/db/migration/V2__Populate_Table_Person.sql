c9e6048c58b3ccba24f2e4cb8d38fe42e3e3f615
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-08-17 17:37:46 -0300

    build: add flyway migration on project

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ana', 'Female', 'Flavia'),
+	(2, 'Rio de Janeiro - Brasil', 'Felipe', 'Male', 'Osorio'),
+	(3, 'Belo Horizonte - Brasil', 'Bruno', 'Male', 'Nogueira'),
+	(4, 'Manaus - Brasil', 'Ana', 'Female', 'Nogueira'),
+	(5, 'Santa Catarina - Brasil', 'Viviane', 'Female', 'Araujo'),
+	(6, 'Mato Grosso do Sul - Brasil', 'Vitor', 'Male', 'Viana'),
+	(7, 'Brasilia - Brasil', 'Richards', 'Male', 'Silva');
